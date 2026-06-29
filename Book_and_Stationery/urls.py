25c673a3e2ce86fa535682cd1efb8a7803d37b92
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-06-29 18:46:57 +0500

    Add URL patterns for admin and accounts

--- Book_and_Stationery/urls.py
+++ Book_and_Stationery/urls.py
@@ -1,1 +1,14 @@
+"""Book_and_Stationery URL configuration."""
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('items.urls')),
+    path('accounts/', include('items.urls')), 
+]
+
+
+
 
